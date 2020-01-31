04735306f69582c5fdd108957cb596a14de561e4
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-01-31 14:39:19 +0100

    Add user components

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UserList from "../components/UserList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "John Doe",
+      image: "https://i.pravatar.cc/300?img=54",
+      places: 3
+    }
+  ];
+
+  return <UserList items={USERS} />;
 };
 
 export default Users;
